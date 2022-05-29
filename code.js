f620eeac66d1d3f1ecbdb6ad829df7c174a6dc1b
Ricardo Del Valle <rickydelvalle96@gmail.com>
2022-05-29 15:45:37 -0400

    Add text to screen after buttons are clicked

--- code.js
+++ code.js
@@ -17,31 +17,105 @@ const minusButton = document.querySelector('#minusB');
 const enterButton = document.querySelector('#enterB');
 const clearButton = document.querySelector('#clearB');
 
-
-
-function printOne(){
+oneButton.addEventListener('click', function() {
     const screen = document.querySelector('.screen');
     screen.innerHTML += '1';
-}
+});
 
-function printTwo(){
+twoButton.addEventListener('click', function(){
     const screen = document.querySelector('.screen');
     screen.innerHTML += '2';
-}
+});
 
-function printAdd(){
+threeButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '3';
+});
+
+fourButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '4';
+});
+
+fiveButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '5';
+});
+
+sixButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '6';
+});
+
+sevenButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '7';
+});
+
+eightButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '8';
+});
+
+nineButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '9';
+});
+
+zeroButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '0';
+});
+
+decimalButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '.';
+    decimalButton.disabled = true;
+});
+
+negativeButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '-';
+    negativeButton.disabled = true;
+});
+
+divideButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '/';
+    decimalButton.disabled = false;
+    negativeButton.disabled = false;
+});
+
+multiplyButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '*';
+    decimalButton.disabled = false;
+    negativeButton.disabled = false;
+});
+
+addButton.addEventListener('click', function(){
     const screen = document.querySelector('.screen');
     screen.innerHTML += '+';
-}
+    decimalButton.disabled = false;
+    negativeButton.disabled = false;
+});
 
-function printClear(){
+minusButton.addEventListener('click', function(){
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '-';
+    decimalButton.disabled = false;
+    negativeButton.disabled = false;
+});
+
+enterButton.addEventListener('click', function(){ //for later
+    const screen = document.querySelector('.screen');
+    screen.innerHTML += '2';
+});
+
+clearButton.addEventListener('click', function(){
     const screen = document.querySelector('.screen');
     screen.innerHTML = '';
-}
-
-
-oneButton.addEventListener('click', printOne);
-twoButton.addEventListener('click', printTwo);
-addButton.addEventListener('click', printAdd);
-clearButton.addEventListener('click', printClear);
+    decimalButton.disabled = false;
+    negativeButton.disabled = false;
+});
 
